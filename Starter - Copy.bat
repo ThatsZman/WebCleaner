c9d70608a08b495c6039bb7657ab3b27cb0d8ec2
ThatsZman <zacharychaussee@gmail.com>
2018-02-19 17:42:25 -0500

    Add files via upload

---
+++ Starter - Copy.bat
@@ -0,0 +1,33 @@
+@echo off
+color 0e
+title Chrome WebWasher
+
+echo Refreshing Google Chrome.exe
+
+
+
+
+timeout 5 >nul
+
+
+echo Refreshing Google Chrome.exe/sia
+
+echo Opening Scanning Tools (chromeclearscan.exe)
+
+timeout 5 >nul
+
+echo Running System Cover Load
+
+
+timeout 3
+
+
+
+
+
+
+
+
+
+
+start C:\Users\zacha\Desktop\ScrnWasher\question.vbs
