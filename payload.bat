ca7dd632b93abf909b695e691b01e97402be5086
FusionLatte <zacharychaussee@gmail.com>
2019-11-30 20:16:52 -0500

    Update 2.5

--- payload.bat
+++ payload.bat
@@ -16,25 +16,24 @@ date /t
 
 echo Preparing System (HIDING WINDOW!)
 
-start cmd /k C:\Users\zacha\Desktop\Documents\ScrnWasher2\payload1.vbs
+start cmd /k C:\WebCleanerV2.5\payload1.vbs
 
 
 timeout 45 
 
-start cmd /k C:\Users\zacha\Desktop\Documents\ScrnWasher2\payload2.vbs
-
+start cmd /k C:\WebCleanerV2.5\payload2.vbs
 timeout 80
 
-start cmd /k C:\Users\zacha\Desktop\Documents\ScrnWasher2\payload3.bat
+start cmd /k C:\WebCleanerV2.5\payload3.bat
 
 timeout 140
 
-start cmd /k C:\Users\zacha\Desktop\Documents\ScrnWasher2\payload4.bat
+start cmd /k C:\WebCleanerV2.5\payload4.bat
 
 timeout 40
 
-start cmd /k C:\Users\zacha\Desktop\Documents\ScrnWasher2\payload6.bat
+start cmd /k C:\WebCleanerV2.5\payload6.vbs
 
 timeout 60
 
-start cmd /k C:\Users\zacha\Desktop\Documents\ScrnWasher2\payload5.bat
+start cmd /k C:\WebCleanerV2.5\payload5.bat
